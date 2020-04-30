5f5b3a55054420049c4994e1b4297c0cefb75901
GruberLevente <64478756+GruberLevente@users.noreply.github.com>
2020-04-30 17:56:07 +0200

    add third part

--- scripts.js
+++ scripts.js
@@ -1,13 +1,23 @@
 // Third part:
-
-$('h1').html('This is done by <em>JavaScript</em>');
-$('p').css('border', '1px solid red');
-$('p').css('padding', '10px');
-$('h2').toggleClass('blue');
-$('h2').toggleClass('blue');
-$('main').append('<p>A p is appended in the main</p>');
-
 console.log('Script file is loaded');
+
+$('h1').html('This is done by <em>JavaScript!</em>');
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue');
+
+let name = 'Levi';
+let age = 20;
+
+function myLittleFunction() {
+    $('main').append('<p>A p is appended in the main</p>');
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '10px');
+}
+
+$('#add').click(myLittleFunction);
+$('#remove').click(function () {
+    $('p:last-child').remove();
+});
 
 // Single line comment
 
